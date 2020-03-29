beb1f103abf6ae27664ad39f83dd7f60c6e51169
Zaksapthawee <i-am-zak_189@hotmail.com>
2020-03-29 13:31:52 +0700

    admin_feagment_1

--- StuActivity/app/src/main/java/th/ac/kku/cis/mobileapp/stuactivity/AdminMain.kt
+++ StuActivity/app/src/main/java/th/ac/kku/cis/mobileapp/stuactivity/AdminMain.kt
@@ -1,15 +1,50 @@
 package th.ac.kku.cis.mobileapp.stuactivity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_admin_main.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class AdminMain : AppCompatActivity(){
+    lateinit var auth: FirebaseAuth
+    lateinit var googleClient: GoogleSignInClient
+    var boo:Boolean = false
+    private val PERMISSION_CODE = 1000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_admin_main)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                //permission was not enabled
+                val permission = arrayOf( Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                //show popup to request permission
+                requestPermissions(permission, PERMISSION_CODE)
+            }
+            else{
+
+            }
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
     }
+
+
 }
