416bfa0bbe8f3669f7e2c4c7eb93def1d4b4633c
Zaksapthawee <i-am-zak_189@hotmail.com>
2020-04-01 01:09:42 +0700

    Update Student_Activity.kt

--- StuActivity/app/src/main/java/th/ac/kku/cis/mobileapp/stuactivity/View/Student_Activity.kt
+++ StuActivity/app/src/main/java/th/ac/kku/cis/mobileapp/stuactivity/View/Student_Activity.kt
@@ -6,52 +6,31 @@ import android.util.Log
 import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_admin_main.*
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.android.gms.common.api.ApiException
+import kotlinx.android.synthetic.main.activity_admin.*
 import th.ac.kku.cis.mobileapp.stuactivity.R
 
-class Student_Activity : AppCompatActivity(){
+class Student_Activity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     lateinit var googleClient: GoogleSignInClient
     var boo: Boolean = false
     private val PERMISSION_CODE = 1000;
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        setContentView(R.layout.activity_student_main)
-
-            }
-        }
-    }
-    private fun firebaseAuth(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    boo = true
-
-            var i = Intent(this, Save_Activity::class.java)
-            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(i)
-        }
-    }
-    private fun passproject() {
-        if (boo) {
-            var i = Intent(this, MainActivity::class.java)
-            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            startActivity(i)
-        }
-    }
-    private fun singOut() {
-
-        auth.signOut()
-        boo = true
-        passproject()
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_student)
     }
 }
+
+
